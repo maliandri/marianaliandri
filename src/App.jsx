385eb82bca2568de4ec75feb9266dff1b56a0da2
maliandri <maliandri@outlook.com>
2025-08-01 12:09:49 -0300

    version 010825

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Hero from "./components/Hero";
 import Proyectos from "./components/Proyectos";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-// Importar otros componentes que crees
+import "./index.css";
 
+// El componente principal de la aplicación, el cual renderiza los demás componentes
 export default function App() {
   return (
-    <main className="max-w-4xl mx-auto p-4 space-y-16">
-      <Hero />
-      <Proyectos/>
-      <Skills />
-      <Contact />
-      {/* Puedes agregar aquí otros componentes, como la sección de CV */}
+    <main className="font-sans min-h-screen text-gray-800 bg-gray-50 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-500">
+      <div className="max-w-5xl mx-auto p-4 space-y-20">
+        <Hero />
+        <Proyectos />
+        <Skills />
+        <Contact />
+      </div>
     </main>
   );
 }
